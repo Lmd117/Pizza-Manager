54f5298f67aa0e2d942c33cf65ec37bd14201510
Mariana <lmd117@case.edu>
2025-02-09 18:29:36 -0500

    Frontend Test

---
+++ src/mainpage.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import {
+  Button,
+  Heading,
+  Flex,
+  View,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+
+function App () {
+
+
+    return (
+        <Flex
+            className="App"
+            justifyContent="center"
+            alignItems="center"
+            direction="column"
+            width="90%"
+            margin="0 auto">
+            <Flex>
+
+            </Flex>
+            <Flex>
+
+            </Flex>
+        </Flex>
+    )
+}
