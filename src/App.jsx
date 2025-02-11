82fe37dc87e45378d981c9e2ec3acdc3ed243021
Mariana <lmd117@case.edu>
2025-02-10 20:14:23 -0500

    Authenticator Clean Up
    Hoping that this is the last of the authenticator template

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,14 @@ import {
   View,
   Heading,
 } from "@aws-amplify/ui-react";
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import { generateClient } from "aws-amplify/data";
-import outputs from "../amplify_outputs.json";
 import './App.css'
 import PizzaPage from './PizzaPage';
 import ToppingsPage from './ToppingsPage';
 import Header from './Header';
 
 
-
-Amplify.configure(outputs);
-const client = generateClient({
-  authMode: "userPool",
-});
-
-
 function App() {
-  const [userprofiles, setUserProfiles] = useState([]);
-  const { signOut } = useAuthenticator((context) => [context.user]);
-
-  useEffect(() => {
-    fetchUserProfile();
-  }, []);
-
-  async function fetchUserProfile() {
-    const { data: profiles } = await client.models.UserProfile.list();
-    setUserProfiles(profiles);
-  }
 
   return (
     <BrowserRouter>
