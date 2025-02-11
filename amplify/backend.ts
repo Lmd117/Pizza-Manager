1a926dd776c0b25b1fc33cb49482633b202dcc70
Mariana <lmd117@case.edu>
2025-02-10 19:19:21 -0500

    Fixing host bug
    the backend.ts had a lambda function included that it shouldn't have had

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { PizzaToppingsLambda } from './functions/PizzaToppingsLambda/resource';
 
 defineBackend({
   auth,
   data,
-  PizzaToppingsLambda,
 });
